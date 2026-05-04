769fa3b4514f9ae60be758eeee687304a02bdf0a
yasmin <yasmimarinhofalcao@gmail.com>
2026-05-04 19:01:04 -0300

    refactor: melhora as funções que lidam com o feedback

--- src/main.js
+++ src/main.js
@@ -3,16 +3,18 @@ import { handleLogin, btnLogin } from '/pages/login/main.js';
 import { renderizarPerfil, renderizarBotãoGithub, conectarGithub } from '/pages/perfil/main.js';
 import '/src/style.css'
 import { supabase } from '/src/supabaseClient.js'
+import { setStorage } from './lib/storage.js'
 /*
 import javascriptLogo from './assets/javascript.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import { setupCounter } from './counter.js'
 */
+
 // função de logout
 function fazerLogout() {
     const linkLogout = document.getElementById('link-logout');
-    
+
     if (linkLogout) {
         linkLogout.addEventListener('click', async (e) => {
             e.preventDefault();
@@ -50,15 +52,13 @@ function inserirHtml(id, caminho) {
             .then(response => {
                 if (!response.ok) throw new Error("Erro ao carregar o componente");
                 return response.text();
-            })
-            .then(html => {
+            }).then(html => {
                 idElemento.innerHTML = html;
                 if (id === 'navbar') {
                     configurarMenuHamburguer();
                     fazerLogout();
                 }
-            })
-            .catch(error => console.error(error));
+            }).catch(error => console.error(error));
     }
 };
 
@@ -74,38 +74,45 @@ function handleForm(form) {
     }
 })};
 
-// função para lidar com o feedback dos usuários
-async function handleFeedback() {
-    const formFeedback = document.getElementById('feedback');
-    const camposFeedback = document.getElementById('hidden-feedback');
-    const btnFeedback = document.getElementById('btn-feedback');
-    const msgFeedback = document.getElementById('msg-feedback');
-    const submitFeedback = document.getElementById('submit-feedback');
-    if (camposFeedback.style.display === 'none' || camposFeedback.style.display === '') {
-        camposFeedback.style.display = 'block';
-    } else {
-        camposFeedback.style.display = 'none';
-    }
-    async function getEmail() {
+// função para obter o email do usuário logado
+async function getEmail() {
         const { data, error } = await supabase.auth.getUser();
+
         if (data.user) {
             return data.user.email;
         } if (error) {
             alert('Faça login para enviar seu feedback!');
             window.location.href = '/pages/login/';
+            return null;
         }
-    } const email = await getEmail();
-    if (btnFeedback && msgFeedback && submitFeedback && formFeedback) {
+}
+
+// função para lidar com o feedback dos usuários
+async function handleFeedback(email) {
+    const formFeedback = document.getElementById('feedback');
+    const camposFeedback = document.getElementById('hidden-feedback');
+    const btnFeedback = document.getElementById('btn-feedback');
+    const msgFeedback = document.getElementById('msg-feedback');
+    const submitFeedback = document.getElementById('submit-feedback');
+
+    if (camposFeedback.style.display === 'none' || camposFeedback.style.display === '') {
+        camposFeedback.style.display = 'block';
+    } else {
+        camposFeedback.style.display = 'none';
+    } if (btnFeedback && msgFeedback && submitFeedback && formFeedback) {
         formFeedback.addEventListener('submit', (e) => {
             e.preventDefault();
-            if (msgFeedback.value.length < 10 || msgFeedback.value.length > 500) {
+            if (msgFeedback.value.trim() === '' ||  msgFeedback.value.length < 10 || msgFeedback.value.length > 500) {
                 alert('Por favor, escreva uma mensagem com no mínimo 10 caracteres e no máximo 500.');
             } else {
-                localStorage.setItem(email, JSON.stringify(msgFeedback.value));
-                alert('Feedback enviado com sucesso! Agradecemos por compartilhar sua opinião conosco.');
-                msgFeedback.value = '';
-                camposFeedback.style.display = 'none';
-            }
+                const confirmar = confirm('Tem certeza que deseja enviar seu feedback?');
+                if (confirmar) {
+                    setStorage(email, msgFeedback.value);
+                    alert('Feedback enviado com sucesso! Agradecemos por compartilhar sua opinião conosco.');
+                    msgFeedback.value = '';
+                    camposFeedback.style.display = 'none';
+                }
+            } 
         });
     }
 };
@@ -125,6 +132,9 @@ btnLogin?.addEventListener('click', () => {
     handleForm(document.querySelector('#form-login'));
 });
 
-document.getElementById('btn-feedback')?.addEventListener('click', () => {
-    handleFeedback();
+document.getElementById('btn-feedback')?.addEventListener('click', async () => {
+    const email = await getEmail();
+    if (email !== null) {
+        handleFeedback(email);
+    }
 });
